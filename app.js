da69010630721cbb64fff7b76ae64022e4e8ba5d
Dipikesh <dipikesh.singh.915@gmail.com>
2021-09-21 16:47:06 +0530

    Winston Logger is added

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const cors = require("cors");
 const helmet = require("helmet");
 require(`dotenv`).config();
 const app = express();
-
+const logger = require("./config/logger");
+const httpLogger = require("./config/httpLogger");
 
 
 
 app.use(helmet());
+app.use(helmet());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,9 +35,20 @@ app.use(async (err, req, res, next) => {
     })
 });
 
+
+
 const PORT =  process.env.PORT || 8000
 const server = app.listen(PORT, () => {
- console.log("Server is runnin on port 8000");
+ logger.info(`Server is runnin on port ${PORT}`);
+});
+
+process.once('SIGUSR2', function () {
+  process.kill(process.pid, 'SIGUSR2');
+});
+
+process.on('SIGINT', function () {
+  // this is only called on ctrl+c, not restart
+  process.kill(process.pid, 'SIGINT');
 });
 
 module.exports = server;
---
+++ config/logger.js
@@ -0,0 +1,64 @@
+const winston = require('winston');
+const colorizer = winston.format.colorize();
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const level = () => {
+  const env = process.env.NODE_ENV || 'dev'
+  const isDevelopment = env === 'dev'
+  return isDevelopment ? 'debug' : 'warn'
+}
+
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'blue',
+  http: 'magenta',
+  debug: 'white',
+}
+
+winston.addColors(colors)
+// Chose the aspect of your log customizing the log format.
+const format = winston.format.combine(
+  // Add the message timestamp with the preferred format
+  winston.format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' }),
+  winston.format.simple(),
+  // Tell Winston that the logs must be colored
+  // winston.format.colorize({ all: true }),
+  // Define the format of the message showing the timestamp, the level and the message
+  winston.format.printf(msg => 
+    colorizer.colorize(msg.level, `${msg.timestamp} - [${msg.level}] : ${msg.message}`)
+  )
+)
+
+// Define which transports the logger must use to print out messages. 
+// In this example, we are using three different transports 
+const transports = [
+  // Allow the use the console to print the messages
+  new winston.transports.Console(),
+  // Allow to print all the error level messages inside the error.log file
+  new winston.transports.File({
+    filename: 'logs/error.log',
+    level: 'error',
+  }),
+  // Allow to print all the error message inside the all.log file
+  // (also the error log that are also printed inside the error.log(
+  new winston.transports.File({ filename: 'logs/all.log' }),
+]
+
+// Create the logger instance that has to be exported 
+// and used to log messages.
+const Logger = winston.createLogger({
+  level: level(),
+  levels,
+  format,
+  transports,
+})
+
+module.exports = Logger
