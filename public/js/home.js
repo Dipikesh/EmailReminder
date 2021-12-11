b61f3282428d9b72853d28a055f5d063726306c7
dipikesh <dipikesh.singh.915@gmail.com>
2021-12-11 13:27:41 +0530

    frontend bug fixed

--- public/js/home.js
+++ public/js/home.js
@@ -178,7 +178,7 @@ async function fetchTaskList() {
     deleteButton.innerText = "Delete";
     deleteButton.value = task._id;
     deleteButton.classList.add("btn");
-    deleteButton.className='delButton';
+    deleteButton.className='deleteButton';
 
     editButton.innerText = "Edit";
     editButton.value = task._id;
